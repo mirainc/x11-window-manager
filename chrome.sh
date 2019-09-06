71de3c7bfc5810e692d01e2f7e26e2ec3af7f1cf
Aric Huang <arichuang@gmail.com>
2019-09-06 16:50:27 -0700

    blah

---
+++ chrome.sh
@@ -0,0 +1,2 @@
+# launch chrome
+runuser -l chrome -c "DISPLAY=$DISPLAY /usr/bin/google-chrome --no-first-run"
--- start_x86.sh
+++ start_x86.sh
@@ -4,8 +4,23 @@ if [[ -z "$DISPLAY" ]]; then
     export DISPLAY=:0.0
 fi
 
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# rotate screen if env variable is set [normal, inverted, left or right]
+if [[ ! -z "$ROTATE_DISPLAY" ]]; then
+  echo "YES"
+  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) &
+fi
+
+# start desktop manager
+echo "STARTING X"
+
+# allow X-windows apps access for *any* user on the system
+xhost +SI:localuser:chrome
+
 if [[ "$DISPLAY" == "host.docker.internal:0" && "$UDEV" -eq 0 ]]; then
-    bash /usr/src/app/.xinitrc
+    xterm &
+    bash ./chrome.sh
 else
-    startx /usr/src/app/.xinitrc
+    startx
 fi
