eeac07049ba9e30a83ac05f9077ee439ec607162
David Phipps <david@getmira.com>
2019-09-06 14:35:02 -0700

    adding chrome

--- start_x86.sh
+++ start_x86.sh
@@ -6,15 +6,24 @@ export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
 # rotate screen if env variable is set [normal, inverted, left or right]
 if [[ ! -z "$ROTATE_DISPLAY" ]]; then
   echo "YES"
-  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) &
+  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) & 
 fi
 
 # start desktop manager
 echo "STARTING X"
-# startx
+startx xterm
 
-# uncomment to start x without mouse cursor
-# startx -- -nocursor
+# allow X-windows apps access for *any* user on the system
+xhost +
+
+# enable X11 forwarding
+ssh chrome@$HOSTNAME -X
+
+# enable trusted X11 forwarding for this specific user
+ssh chrome@$HOSTNAME -Y
 
 # uncomment to open an application instead of the desktop
-startx xterm
+# startx xterm  
+
+# launch chrome
+runuser -l  chrome -c '/opt/google/chrome/google-chrome --user-data-dir=/data'
